4681278f25dd21da3886c65b35951234f3548d0a
Sergei Kibardin <kibardinsa@gmail.com>
2023-09-26 14:46:30 +0300

    implement HW_FINAL_2

---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,22 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        // Целое число в полуинтервале (0, 100]
+        int mysteryNum = 10;
+        int playerNum = 1;
+
+        if (!(playerNum > 0 && playerNum <= 100)) {
+            System.out.println("Введите целое число в полуинтервале (0, 100]"); 
+        } else {
+            while (playerNum != mysteryNum) {
+                if (playerNum < mysteryNum) {
+                    System.out.println("Число " + playerNum + " меньше того, что загадал компьютер");
+                    playerNum ++;
+                } else {
+                    System.out.println("Число " + playerNum + " больше того, что загадал компьютер");
+                    playerNum --;
+                }
+            }
+            System.out.println("Вы победили!"); 
+        }
+    }
+}

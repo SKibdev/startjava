60de8e729dff01d5b64fb004193ad80beb8fb506
Sergei Kibardin <kibardinsa@gmail.com>
2023-10-02 17:37:33 +0300

    implement HW_1 lesson_2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    
+    String gender = "male";
+    String name = "Ivan";
+    double height = 1.75;
+    double weight = 98.5;
+    int age = 18;
+
+    void go() {
+        System.out.println("идти");
+    }
+
+    void sit() {
+        System.out.println("сидеть");
+    }
+
+    void run() {
+        System.out.println("бежать");
+    }
+
+    void talk() {
+        System.out.println("говорить");
+    }
+
+    void learnJava() {
+        System.out.println("учить Java");
+    }
+}

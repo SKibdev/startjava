f6919477690e9e0c8d96251a99ae2d51adadd515
Sergei Kibardin <kibardinsa@gmail.com>
2023-10-04 09:17:34 +0300

    implement HW_3 lesson_2

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,15 +2,14 @@ public class WolfTest {
     
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        wolfOne.gender = "female";
-        wolfOne.nickname = "Bad";
-        wolfOne.weight = 27.5f;
-        wolfOne.age = 5;
-        wolfOne.color = "gray";
-
-        System.out.println("gender: " + wolfOne.gender + "\nnickname: " + wolfOne.nickname + 
-                "\nweight: " + wolfOne.weight + "\nage: " + wolfOne.age + 
-                "\ncolor: " + wolfOne.color);
+        wolfOne.setGender("female");
+        wolfOne.setNickname("Bad");
+        wolfOne.setWeight(27.5f);
+        wolfOne.setAge(5);
+        wolfOne.setColor("gray");
+        System.out.println("gender: " + wolfOne.getGender() + "\nnickname: " + 
+            wolfOne.getNickname() + "\nweight: " + wolfOne.getWeight() + "\nage: " + wolfOne.getAge() + 
+                "\ncolor: " + wolfOne.getColor());
         wolfOne.go();
         wolfOne.sit();
         wolfOne.run();

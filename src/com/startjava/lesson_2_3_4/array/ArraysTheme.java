ed85db76a0284d7a9581d9a55e12038a4a0e8a0d
Sergei Kibardin <kibardinsa@gmail.com>
2023-11-04 21:59:36 +0300

    HW_1 lesson_4 reverse(), multiplicate(),displayAlphabetLadder() modified.

--- src/com/startjava/lesson_2_3_4/array/ArraysTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArraysTheme.java
@@ -2,45 +2,55 @@ package com.startjava.lesson_2_3_4.array;
 
 public class ArraysTheme {
     public static void main(String[] args) {
-        executeReverse();
-        executeMultiplication();
+        reverse();
+        multiplicate();
         deleteElements();
         displayAlphabetLadder();
         fillUniqueNumbers();
     }
 
-    public static void executeReverse() {
+    public static void reverse() {
         System.out.println("1. Реверс значений массива");
 
-        int[]  numbers = {1, 7, 4, 5, 2, 6, 3};
-        int length = numbers.length;
+        int[] reverseNums = {1, 7, 4, 5, 2, 6, 3};
+        int length = reverseNums.length;
 
         System.out.print("   До реверса: ");
-        displayNumbers(numbers);
+        display(reverseNums);
 
-        for (int i = 0, j = length - 1; i < (length - 1)/2; i++, j--) {
-            int temporary = numbers[i];
-            numbers[i] = numbers[j];
-            numbers[j] = temporary;
+        for (int i = 0; i < length; i++) {
+            int tmp = reverseNums[i];
+            reverseNums[i] = reverseNums[length - 1];
+            reverseNums[length - 1] = tmp;
+            length--;
         }
 
         System.out.print("После реверса: ");
-        displayNumbers(numbers);
+        display(reverseNums);
     }
 
-    public static void executeMultiplication() {
+    private static void display(int[] numbers) {
+        int length = numbers.length;
+        System.out.print("[");
+        for (int i = 0; i < length; i++) {
+            System.out.print(numbers[i] + ", ");
+        }
+        System.out.print(numbers[length - 1] +  "]\n");
+    }
+
+    public static void multiplicate() {
         System.out.println("\n2. Произведение элементов массива");
 
-        int[] numbers1 = new int[10];
-        int length = numbers1.length;
-        for (int i = 0; i <= length - 1; i++) {
-            numbers1[i] = i;
+        int[] multipliedNums = new int[10];
+        int length = multipliedNums.length;
+        for (int i = 0; i < length; i++) {
+            multipliedNums[i] = i;
         }
 
         int multiplication = 1;
-        for (int i = 1; i <= length - 2; i++) {
-            multiplication *= numbers1[i];
-            System.out.print(i == length - 2 ? (numbers1[i] + " = " + multiplication) : (numbers1[i] + " * "));
+        for (int i = 1; i < length - 1; i++) {
+            multiplication *= multipliedNums[i];
+            System.out.print(multipliedNums[i] + (i != length - 2 ? (" * ") : (" = " + multiplication)));
         }
     }
 
@@ -49,7 +59,7 @@ public class ArraysTheme {
 
         double[] numbers2 = new double[15];
         int length = numbers2.length;
-        for (int i = 0; i <= length - 1; i++) {
+        for (int i = 0; i < length; i++) {
             numbers2[i] = Math.random();
         }
 
@@ -58,7 +68,7 @@ public class ArraysTheme {
 
         double middle = numbers2[length/2];
         int counter = 0;
-        for (int i = 0; i <= length - 1; i++) {
+        for (int i = 0; i < length; i++) {
             if (numbers2[i] > middle) {
                 numbers2[i] = 0;
                 counter++;
@@ -71,19 +81,31 @@ public class ArraysTheme {
         System.out.println("\nКоличество обнуленных ячеек = " + counter);
     }
 
+    private static void displayNumbers2(double[] numbers2) {
+        int i = 0;
+        for (double number : numbers2) {
+            if (i == 7) {
+                System.out.printf("%.3f \n", number);
+            } else {
+                System.out.printf("%.3f ", number);
+            }
+            i++;
+        }
+    }
+
     public static void displayAlphabetLadder() {
         System.out.println("\n4.Вывод алфавита лесенкой");
 
-        char[] letters = new char[26];
-        int length = letters.length;
-        int index = 0;
-        for (char ch = 'A'; ch <= 'Z'; ch++) {
-            letters[index++] = ch;
+        char[] alphabet = new char[26];
+        int length = alphabet.length;
+
+        for (int i = 0; i < length; i++) {
+            alphabet[i] = (char) ('A' + i);
         }
 
         for (int i =  length - 2; i >= -1; --i) {
             for (int j = length - 1; j > i; --j) {
-                System.out.print(letters[j]);
+                System.out.print(alphabet[j]);
             }
             System.out.println();
         }
@@ -95,7 +117,7 @@ public class ArraysTheme {
         int[] numbers3 = new int[30];
         int length = numbers3.length;
 
-        for (int i = 0; i <= length-1; i++) {
+        for (int i = 0; i < length; i++) {
             numbers3[i] = (int) (Math.random() * 40 + 60);
             int index = 0;
             while (index < i) {
@@ -109,37 +131,11 @@ public class ArraysTheme {
 
         sortBubble(numbers3);
 
-        for (int i = 0; i <= length - 1; i++) {
+        for (int i = 0; i < length; i++) {
             System.out.print(numbers3[i] + " ");
             if ((i - 9) % 10 == 0) {
                 System.out.println();
             }
-        }
-    }
-
-    private static void displayNumbers(int[] numbers) {
-        int i = 0;
-        for (int number : numbers) {
-            if (i == 0) {
-                System.out.print("[" + number + ",");
-            } else if (i == numbers.length-1) {
-                System.out.print(number + "]\n");
-            } else {
-                System.out.print(number + ",");
-            }
-            i++;
-        }
-    }
-
-    private static void displayNumbers2(double[] numbers2) {
-        int i = 0;
-        for (double number : numbers2) {
-            if (i == 7) {
-                System.out.printf("%.3f \n", number);
-            } else {
-                System.out.printf("%.3f ", number);
-            }
-            i++;
         }
     }
 

700118a70016a954384fb7ff9e7f1b8fef1c3884
Sergei Kibardin <kibardinsa@gmail.com>
2023-11-23 20:01:22 +0300

    HW_Final lesson_4  Calculator* corrected from code review dated 23/11/2023

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -4,13 +4,12 @@ import java.lang.Math;
 
 public class Calculator {
 
-    private static final int expressionLength = 3;
-
+    private static final int EXPRESSION_LENGTH = 3;
     private static int num1;
     private static int num2;
     private static char sign;
 
-    public static double  calculate(String expression) {
+    public static double calculate(String expression) {
         parse(expression);
         return switch (sign) {
             case '+' -> num1 + num2;
@@ -25,18 +24,18 @@ public class Calculator {
 
     private static void parse(String expression) {
         String[] elements = expression.split(" ");
-        if (elements.length != expressionLength) {
+        if (elements.length != EXPRESSION_LENGTH) {
             throw new RuntimeException("Ошибка!!! Мат. выражение не в формате \"А + В\"");
         }
         sign = elements[1].charAt(0);
         try {
             num1 = Integer.parseInt(elements[0]);
             num2 = Integer.parseInt(elements[2]);
-            if (num1 <= 0 || num2 <= 0) {
-                throw new RuntimeException("Ошибка!!! Числа должны быть положительными!");
-            }
         } catch (NumberFormatException ex) {
             throw new NumberFormatException("Ошибка!!! Операции возможны только над целыми числами!");
+        }
+        if (num1 <= 0 || num2 <= 0) {
+            throw new RuntimeException("Ошибка!!! Числа должны быть положительными!");
         }
     }
 }
